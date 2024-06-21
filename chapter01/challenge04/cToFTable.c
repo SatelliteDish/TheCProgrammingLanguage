ac79571854b8426556bf7b081ebeb5a468ff9f32
Michael V <michael.a.j.vitale@gmail.com>
2024-06-21 09:00:35 +0000

    did first 4 challenges

---
+++ chapter01/challenge04/cToFTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+  printf(" C\tF \n-------------\n");
+  celsius = lower;
+  while(celsius <= upper) {
+    fahr = (celsius * (9.0/5.0)) + 32;
+    printf("%d\t%d\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
